0ba4f93f2a47ff7ebdfce9fbe08aa391a7d001f3
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-01 18:25:50 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -4,6 +4,17 @@ public class Funcionario extends Pessoa{
 
     private double salario;
 
+    // metodo sobreescrito
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
+    /* um metodo sobreescrito é um metodo que redefine o comportamento de um metodo
+       herdado,  ela possui o mesmo nome e os mesmos parametros
+
+       O super. em um método sobrescrito chama a versão original do método na classe */
+
     public double getSalario() {
         return salario;
     }
