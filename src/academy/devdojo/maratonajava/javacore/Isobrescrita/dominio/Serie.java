29d7c94f84e63cbb80b15ae7c565e5d73ea083b1
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-02 19:47:39 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/dominio/Serie.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.dominio;
+
+public class Serie {
+
+    private String nome;
+
+    public Serie(String nome) {
+        this.nome = nome;
+    }
+
+    // sobrescrita do metodo toString()
+    @Override
+    public String toString() {
+        return "Serie{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+
+    /* o método toString() em Java é um método herdado da classe Object e é
+       usado para retornar uma representação em formato string de um objeto, aqui ele
+       esta sendo sobrescrito para fornecer uma melhor descrição
+
+       para verificar se esta usando coretamente a sobrescrita neste metodo usar
+       o @Override e prestar atenção no nome */
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/test/SerieTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescrita.dominio.Serie;
+
+public class SerieTest01 {
+    public static void main(String[] args) {
+
+        Serie serie = new Serie("Breaking Bad");
+
+        System.out.println(serie);
+
+    }
+}
