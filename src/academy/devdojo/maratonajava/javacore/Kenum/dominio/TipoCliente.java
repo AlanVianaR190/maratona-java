38c30da7ec2b1e11c7ab63bbfdbc2b610df6d3c7
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-03 17:36:39 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Kenum.dominio;
+
+public enum TipoCliente {
+
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+
+    /* enumeração é um tipo especial de classe onde todos os atributos são constantes
+       a enumeração e indicada quando em um programa vamos ter varias constantes, ela é
+       otima quando temos uma quantidade limitada  de escolha e precisamos forçar entre
+       essas opções
+
+       */
+}
