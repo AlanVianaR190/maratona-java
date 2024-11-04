65bb114ea40f7ff3ffedd054c3e1a001aa9f93e3
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-04 15:57:29 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -6,4 +6,11 @@ public interface DataLoader {
 
     /* interfaces não podem ser final, metodos em interface já são
        publicos e abstratos por natureza */
+
+    default void checkPermission(){
+        System.out.println("Fazendo checagem de permissões...");
+    }
+
+    /* a partir da versão 8 do Java existe a possibilidade de colocar metodos concretos
+       na interface */
 }
--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -1,12 +1,22 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
-public class DatabaseLoader implements DataLoader{
+public class DatabaseLoader implements DataLoader, DataRemover{
 
     @Override
     public void load() {
         System.out.println("Carregando dados do banco de dados...");
     }
 
+    @Override
+    public void remove() {
+        System.out.println("Removendo do banco de dados...");
+    }
+
     /* quando esta trabalhando com intefaces se utiliza o implements,
-       */
+       e é possivel implementar varias intefaces */
+
+    @Override
+    public void checkPermission() {
+        System.out.println("Checando opções no banco de dados.. ");
+    }
 }
