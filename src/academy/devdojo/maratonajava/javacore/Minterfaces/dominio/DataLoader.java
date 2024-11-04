259808a26b64c48a6af056fbe4874d18cff1d001
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-04 15:33:56 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public interface DataLoader {
+
+    void load();
+
+    /* interfaces não podem ser final, metodos em interface já são
+       publicos e abstratos por natureza */
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+public class DatabaseLoader implements DataLoader{
+
+    @Override
+    public void load() {
+        System.out.println("Carregando dados do banco de dados...");
+    }
+
+    /* quando esta trabalhando com intefaces se utiliza o implements,
+       */
+}
