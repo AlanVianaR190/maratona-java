f3ea818da052c9effbf551599370c4afe660a68c
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-04 19:11:02 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest01 {
+
+    public static void main(String[] args) {
+
+        Computador computador = new Computador("Asus", 3000);
+
+        Tomate tomate = new Tomate("Tomate Italiano",7.99);
+
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        System.out.println("----------------------------------");
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+
+
+    }
+
+    /* camada de VIZUALIZAÇÃO */
+}
