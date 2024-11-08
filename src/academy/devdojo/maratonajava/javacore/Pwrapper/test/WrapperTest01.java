94ba5dc2f2315a439a0239089cdfc307b5159428
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-08 11:39:31 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+
+    public static void main(String[] args) {
+
+        byte byterP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 1;
+        float floatP = 10F;
+        double doubleP = 100;
+        char charP = 'A';
+        boolean booleanP = false;
+
+        // classes wrappers
+        Byte byterA = 1;
+        Short shortA = 1;
+        Integer intA = 1;
+        Long longA = 10L;
+        Float floatA = 10F;
+        Double doubleA = 100D;
+        Character charA = 'A';
+        Boolean booleanA = false;
+
+        /* wrappers são objetos que encapsulam tipos primitivos
+           transformando-os em objetos, os valores dos wrappers não
+           estão mais relacionados ao tipo primitivo e sim a suas classes */
+    }
+}
