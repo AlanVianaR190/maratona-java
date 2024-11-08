6bd1fc32d8b5e3d01b5e0d0bec2a3f7c7890f919
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-08 14:05:19 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -14,6 +14,8 @@ public class WrapperTest01 {
         boolean booleanP = false;
 
         // classes wrappers
+
+        // autoboxing
         Byte byterA = 1;
         Short shortA = 1;
         Integer intA = 1;
@@ -26,5 +28,26 @@ public class WrapperTest01 {
         /* wrappers são objetos que encapsulam tipos primitivos
            transformando-os em objetos, os valores dos wrappers não
            estão mais relacionados ao tipo primitivo e sim a suas classes */
+
+        // unboxing
+        int i = intA;
+
+        /* unboxing é a conversão de um objeto para um tipo primitivo */
+
+        boolean verdadeiro = Boolean.parseBoolean("truE");
+
+        System.out.println(verdadeiro);
+
+        /* os métodos parse...() em Java são usados para converter strings em
+         tipos de dados primitivos, no caso do parseBoolean tudo que não for true independente
+         de maisusculas ou minusculas e considerado false */
+
+        System.out.println(Character.isDigit('1'));
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('b'));
+        System.out.println(Character.isLowerCase('b'));
+        System.out.println(Character.toUpperCase('z'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
