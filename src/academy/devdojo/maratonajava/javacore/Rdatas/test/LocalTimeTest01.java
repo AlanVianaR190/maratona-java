70ab0add0cee79e7fa49e316a8c7738c658f8557
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-11-26 12:50:39 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
 
 public class LocalTimeTest01 {
     public static void main(String[] args) {
@@ -8,7 +9,25 @@ public class LocalTimeTest01 {
         LocalTime time = LocalTime.of(23,59,55);
         LocalTime timeNow = LocalTime.now();
 
+        /* A classe LocalTime é usada em Java para representar um
+           horário do dia, sem levar em conta a data ou o fuso horário. */
+
         System.out.println(time);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+
+        System.out.println(time.get(ChronoField.AMPM_OF_DAY));
+
+        /* ChronoField.AMPM_OF_DAY e uma constanete ela indica se o horário
+           está no período da manhã (AM = 0) ou da tarde (PM = 1). */
+
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+
+        /* MIN & MAX são constantes da classe LocalTime */
+
         System.out.println(timeNow);
+
     }
 }
