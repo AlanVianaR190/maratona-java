6393a27b28bcc51d5d54a1026cd11ea817a93e45
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-12-11 15:40:17 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest01 {
+    public static void main(String[] args) {
+
+        File file = new File("file.txt");
+
+        // lendo um arquivo
+        try (FileReader fileReader = new FileReader(file)) {
+
+            /* char[] in = new char[1];
+             fileReader.read(in);
+            for (char c: in){
+                System.out.println(c);
+            } */
+
+            int i;
+            while ((i = fileReader.read()) != -1){
+                System.out.print(i);
+            }
+
+            /* o metodo .read() le um caracter por vez, por isso aqui ele esta sendo usado
+               em um laço */
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
