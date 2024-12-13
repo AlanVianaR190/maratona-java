570bea73f8680a3c133e46e143263c621a15fc91
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-12-13 16:25:04 -0300

    update

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest02.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest02 {
+    public static void main(String[] args) throws IOException {
+
+        // criando diretorio
+        File diretorio = new File("folder");
+
+        boolean isFolderCreated = diretorio.mkdir();
+        System.out.println(isFolderCreated);
+
+        // criando arquivo no diretorio
+        File ArquivoDiretorio = new File(diretorio,"file.txt");
+
+        boolean isFileCreated = ArquivoDiretorio.createNewFile();
+        System.out.println(isFileCreated);
+
+        // renomeando arquivo
+        File arquivoRenomeado = new File(diretorio,"file_renamed.txt");
+
+        boolean isFileRenamed = ArquivoDiretorio.renameTo(arquivoRenomeado);
+        System.out.println(isFileRenamed);
+
+        // renomeando diretorio
+        File diretorioRenameado = new File("folder_renamed");
+
+        boolean isFolderRenamed = diretorio.renameTo(diretorioRenameado);
+        System.out.println(isFolderRenamed);
+
+
+    }
+}
