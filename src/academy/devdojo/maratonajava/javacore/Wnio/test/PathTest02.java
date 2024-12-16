8e421d8c5af19359a686d2a7ce80d76443af812c
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-12-16 17:19:41 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest02.java
@@ -24,7 +24,8 @@ public class PathTest02 {
         Path subFolderPath = Paths.get("folder/sub_folder/sub_sub_folder");
         Path subFolderDirectory = Files.createDirectories(subFolderPath);
 
-        /**/
+        /* aqui apos criar um objeto idicandando o caminho atraves das pastas, o metodo .createDirectories()
+           verifica se as pastas existe se não existir ele cria até mesmo as intermediarias */
 
         // criando arquivos
         Path filePath = Paths.get(subFolderPath.toString(), "file.txt");
@@ -33,13 +34,18 @@ public class PathTest02 {
             Path filePathCreated = Files.createFile(filePath);
         }
 
-        /**/
+        /* o método .toString() converte o objeto Path (que representa um caminho) em uma String equivalente
+         ao seu caminho em forma de texto */
 
         // copiando um arquivo
         Path source = filePath;
         Path target = Paths.get(filePath.getParent().toString(), "file_renamed.txt");
         Files.copy(source,target, StandardCopyOption.REPLACE_EXISTING);
 
-        /**/
+        /* o objeto source define a origem
+           o objeto  target define o destino da copia e o novo nome do arquivo
+           na ultima linha passamos como parametro os objetos a cim em ordem e
+           a constante StandardCopyOption.REPLACE_EXISTING indica que se já houver
+           um arquivo no destino com o mesmo nome, ele será substituído */
     }
 }
