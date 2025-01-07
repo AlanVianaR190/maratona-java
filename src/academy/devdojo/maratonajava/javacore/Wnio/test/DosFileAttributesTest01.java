35d175d5f4e5c41d520151b19476627eb4753586
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2025-01-07 14:54:11 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/DosFileAttributesTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/DosFileAttributesTest01.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
 
 public class DosFileAttributesTest01 {
     public static void main(String[] args) throws IOException {
@@ -22,7 +24,16 @@ public class DosFileAttributesTest01 {
         Files.setAttribute(path, "dos:readonly", false);
 
         //
+        DosFileAttributes dosFileAttributes = Files.readAttributes(path, DosFileAttributes.class);
+        System.out.println(dosFileAttributes.isHidden());
+        System.out.println(dosFileAttributes.isReadOnly());
 
+        //
+        DosFileAttributeView fileAttributeView = Files.getFileAttributeView(path, DosFileAttributeView.class);
+        fileAttributeView.setHidden(true);
+        fileAttributeView.setReadOnly(true);
+        System.out.println(fileAttributeView.readAttributes().isHidden());
+        System.out.println(fileAttributeView.readAttributes().isReadOnly());
 
     }
 }
